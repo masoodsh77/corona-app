1a657751d3bc4c0e84bfea8ba8b14acee1a4bf4b
unknown <masoodsh77@gmail.com>
2021-08-11 10:22:04 +0430

    make charts

--- src/Components/HomeCard/HomeCard.jsx
+++ src/Components/HomeCard/HomeCard.jsx
@@ -2,53 +2,27 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 
 function HomeCard({ title, data }) {
-    ;
-  const state = {
-    labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+  if(title === "Cases"){  
+    var   color = "rgb(248, 157, 19)"
+  }else if(title === "Deaths"){
+    color = "rgb(190, 49, 68)"
+  }else{
+    color="rgb(0, 159, 157)"
+  }
+  const state = { 
     datasets: [
       {
         label: title,
-        fill: false,
+        fill: true,
         lineTension: 0.5,
-        backgroundColor: "rgba(75,192,192,1)",
+        backgroundColor: color,
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 1,
-        data: [
-          0,
-          5,
-          10,
-          15,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          56,
-          65,
-          59,
-          80,
-          81,
-          56,
-        ],
+        data:data
       },
     ],
 };
-
+console.log(data);
     return (
     <div className="w-100 h-100">
       <div className="bg-dark mt-2 w-100 h-100">
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,26 +3,42 @@ import React, { useEffect, useState } from "react";
 import HomeCard from "../../Components/HomeCard/HomeCard";
 
 function Home() {
-  const [cases , setCases]= useState([])
+  const [cases , setCases]= useState({})
+  const [death , setDeath]= useState({})
+  const [recover , setRecover]= useState({})
+  const [vaccina , setVaccina]= useState({})
   useEffect(()=>{
     axios.get("https://disease.sh/v3/covid-19/historical/iran?lastdays=30")
     .then(res=>{     
       setCases(res.data.timeline.cases)
-      console.log(res.data.timeline.cases);
+      setDeath(res.data.timeline.deaths)
+      setRecover(res.data.timeline.recovered)
+      console.log(res.data);
+    })
+    axios.get("https://disease.sh/v3/covid-19/vaccine/coverage/countries/iran?lastdays=30&fullData=false")
+    .then(res=>{
+      setVaccina(res.data.timeline)
+      console.log(res.data.timeline);
     })
   },[])
+  console.log(cases.length);
+  console.log(death.length);
+  console.log(recover.length);
   return (
     <div className="w-100 h-100">
       <div className="w-100 bg-dark">Covid-19 Iran Information</div>
-      <div className="w-100 h-100 d-flex justify-content-between">
+      <div className="w-100 h-100 d-flex justify-content-between flex-wrap">
       <div className="cards">
           <HomeCard title="Cases" data={cases}/>
         </div>
         <div className="cards">
-          <HomeCard title="Deaths"/>
+          <HomeCard title="Deaths" data={death}/>
         </div>
         <div className="cards">
-          <HomeCard title="Recovered"/>
+          <HomeCard title="Recovered" data={recover}/>
+        </div>
+        <div className="cards">
+          <HomeCard title="Vaccine" data={vaccina}/>
         </div>
       </div>
     </div>
